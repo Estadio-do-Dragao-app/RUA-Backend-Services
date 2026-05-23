60ad6e991a247f84732b10d133ee76468c31fc6a
VM <fanappdragao@gmail.com>
2026-05-23 10:59:04 +0000

    sh change

--- push.sh
+++ push.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-
+eval $(ssh-agent -s)
+ssh-add ~/.ssh/PEI_key
 
 git submodule foreach '
   git checkout -B vm-changes &&
   git add . &&
   git diff --cached --quiet || git commit -m "vm changes" &&
-  git push origin vm-changes &&
-  git log origin/dev..vm-changes --oneline 2>/dev/null | grep -q . && \
-    GH_TOKEN=ghp_o_teu_token_aqui gh pr create --base dev --head vm-changes \
-    --title "VM Changes" --body "Alterações da VM" \
-    --repo $(git remote get-url origin | sed "s/https:\/\/github.com\///") || \
-    echo "Sem alterações em relação à dev, PR ignorado."
+  git push origin vm-changes
+  git checkout main
 '
 
 git add . && git commit -m "update submodule refs" && git push origin main
